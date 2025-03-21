f60765dab7f91d761cdadf568d7551689d87c8e0
gustavoLim4 <gustavolll15567@gmail.com>
2025-03-21 11:26:41 -0300

    templates todos prontos para começar o portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,12 @@ function App() {
   const contador = useSelector((state: RootState) => state.contador.valor);
   const darkMode = useSelector((state: RootState) => state.theme.darkMode);
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-4">Contador: {contador}</h1>
+    <div
+      className={`${
+        darkMode ? "bg-gray-900 text-white" : "bg-white text-black"
+      } h-dvh`}
+    >
+      <h1 className="text-3xl font-bold mb-4">Contador: {contador} </h1>
       <div className="flex gap-2">
         <button
           className="px-4 py-2 bg-blue-500 text-white rounded"
@@ -30,21 +34,19 @@ function App() {
         </button>
         <button
           className="px-4 py-2 bg-green-500 text-white rounded"
-          onClick={() => dispatch(adicionarValor(5))}
+          onClick={() => dispatch(adicionarValor(10))}
         >
-          Adicionar 5
+          Adicionar 10
         </button>
       </div>
       <div
-        className={`${
-          darkMode ? "bg-gray-900 text-white" : "bg-white text-black"
-        } p-8 mt-10 mb10 flex flex-col`}
+        className=""
       >
         <h1 className="text-3xl font-bold mb-5">
           Modo {darkMode ? "Escuro" : "Claro"}
         </h1>
         <button
-          className="px-6 py-2 rounded bg-blue-500 text-white"
+          className="px-6 py-2 rounded bg-blue-500 text-white hover:scale-110 hover:bg-blue-500/70 "
           onClick={() => dispatch(toggleTheme())}
         >
           Alternar Tema
